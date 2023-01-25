1eea49559f88a2b68ef12110b7edc534b99037ad
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-25 08:55:32 +0100

    First commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Holi mi nombre es Elibi</h1>
+      <h2>También me conoceréis como famoso vampiro inventor de los pingüinos:</h2>
+    </div>
+  )
+}
+
+export default App
