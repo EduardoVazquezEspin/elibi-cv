050fdc4f940316b015bdb22944dabce50bb658b2
Eduardo <eduardo.vazquez.7e5@itb.cat>
2023-01-25 09:01:55 +0100

    Pingüinos added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,6 +5,7 @@ function App () {
     <div className='App'>
       <h1>Holi mi nombre es Elibi</h1>
       <h2>También me conoceréis como famoso vampiro inventor de los pingüinos:</h2>
+      <img src={process.env.PUBLIC_URL + '/images/emperorpenguin.webp'} alt='pingüinos' />
     </div>
   )
 }
